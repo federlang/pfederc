385901da937db56e3aa390cf38fee14a6c765a39
Fionn Langhans <fionn.langhans@gmail.com>
2019-11-11 00:06:53 +0100

    Completed basic syntax expressions

--- libs/pfederc_syntax/include/pfederc/expr.hpp
+++ libs/pfederc_syntax/include/pfederc/expr.hpp
@@ -2,14 +2,17 @@
 #define PFEDERC_SYNTAX_EXPR_HPP
 
 #include "pfederc/core.hpp"
+#include "pfederc/lexer.hpp"
 #include "pfederc/errors.hpp"
 
 namespace pfederc {
   enum ExprType {
-    EXPR_TOKEN,
+    EXPR_TOK,
 
-    EXPR_PROGRAM,
+    EXPR_PROGNAME,
     EXPR_USE,
+
+    EXPR_PROG,
     EXPR_FUNC,
     EXPR_LAMBDA,
     EXPR_TRAIT,
@@ -19,13 +22,18 @@ namespace pfederc {
     EXPR_MOD,
     EXPR_SAFE,
     EXPR_IF,
-    EXPR_LOOP,
+    EXPR_LOOP_FOR,
+    EXPR_LOOP_DO,
     EXPR_MATCH,
 
     EXPR_BIOP,
     EXPR_UNOP,
 
     EXPR_BODY,
+
+    EXPR_ARRLIT,
+    EXPR_ARRCPY,
+    EXPR_ARREMPTY,
   };
 
   class Expr;
@@ -46,6 +54,9 @@ namespace pfederc {
   class BiOpExpr;
   class UnOpExpr;
   class BodyExpr;
+  class ArrayCpyExpr;
+  class ArrayLitExpr;
+  class ArrayEmptyExpr;
 
   class Expr {
     const Lexer &lexer;
@@ -58,39 +69,43 @@ namespace pfederc {
      * \param pos Return value of getPosition()
      */
     Expr(const Lexer &lexer, ExprType type, const Position &pos) noexcept;
+    Expr(const Expr &) = delete;
     virtual ~Expr();
 
-    const Lexer &getLexer() const noexcept;
-    ExprType getType() const noexcept;
-    const Position &getPosition() const noexcept;
+    inline const Lexer &getLexer() const noexcept { return lexer; }
+    inline ExprType getType() const noexcept { return type; }
+    inline const Position &getPosition() const noexcept { return pos; }
   };
 
   typedef std::vector<std::unique_ptr<Expr>> Exprs;
 
   class ProgramExpr : public Expr {
-    std::vector<std::string> includes;
+    const Token * progName;
     Exprs imports;
     Exprs defs;
   public:
     /*!\brief Initializes ProgramExpr
+     * \param lexer
      * \param pos Return value getPosition()
+     * \param progName Optional program name, return  value of getProgramName()
      * \param includes Return value of getIncludes()
      * \param imports Return value of getImports()
      * \param defs Return value of getDefinitions()
      */
     ProgramExpr(const Lexer &lexer, const Position &pos,
-        std::vector<std::string> &&includes,
+        const Token *progName,
         Exprs &&imports,
         Exprs &&defs) noexcept;
     virtual ~ProgramExpr();
 
-    const std::vector<std::string> &getIncludes() const noexcept;
-    const Exprs &getImports() const noexcept;
+    inline const Token *getProgramName() const noexcept { return progName; }
+
+    inline const auto &getImports() const noexcept { return imports; }
 
     /*!\return Returns vector of *def* expressions. The expressions
      * are ordered by their corresponding position in Feder code.
      */
-    const Exprs &getDefinitions() const noexcept;
+    inline const auto &getDefinitions() const noexcept { return defs; }
   };
 
   class TokenExpr : public Expr {
@@ -99,16 +114,17 @@ namespace pfederc {
     TokenExpr(const Lexer &lexer, const Token &tok) noexcept;
     virtual ~TokenExpr();
 
-    const Token &getToken() const noexcept;
+    inline const Token &getToken() const noexcept { return tok; }
   };
 
   class UseExpr : public Expr {
     std::unique_ptr<Expr> expr;
   public:
-    UseExpr(const Lexer &lexer, std::unique_ptr<Expr> &&expr) noexcept;
+    UseExpr(const Lexer &lexer, const Position &pos,
+        std::unique_ptr<Expr> &&expr) noexcept;
     virtual ~UseExpr();
 
-    Expr &getExpression() const noexcept;
+    inline Expr &getExpression() const noexcept { return *expr; }
   };
 
   typedef std::tuple<const Token* /* opt varname */,
@@ -124,23 +140,24 @@ namespace pfederc {
   public:
     FuncExpr(const Lexer &lexer, const Position &pos,
       const Token &tokId,
-      Exprs &&templ,
+      Exprs &&templs,
       std::vector<FuncParameter> &&params,
       std::unique_ptr<Expr> &&returnExpr,
       std::unique_ptr<BodyExpr> &&body) noexcept;
     virtual ~FuncExpr();
 
-    const Token &getIdentifier() const noexcept;
-    const Exprs &getTemplates() const noexcept;
-    const std::vector<FuncParameter> &getParameters() const noexcept;
+    inline const Token &getIdentifier() const noexcept { return tokId; }
+    inline const auto &getTemplates() const noexcept { return templs; }
+    inline const auto &getParameters() const noexcept
+    { return params; }
 
     /*!\return Returns optional return expression (return type).
      */
-    const Expr *getReturn() const noexcept;
+    inline const Expr *getReturn() const noexcept { return returnExpr.get(); }
 
     /*!\return Returns optional function body.
      */
-    const Expr *getBody() const noexcept;
+    inline const BodyExpr *getBody() const noexcept { return body.get(); }
   };
 
   class LambdaExpr : public Expr {
@@ -152,8 +169,10 @@ namespace pfederc {
       std::unique_ptr<BodyExpr> &&body) noexcept;
     virtual ~LambdaExpr();
 
-    const Exprs &getParameters() const noexcept;
-    const BodyExpr &getBody() const noexcept;
+    inline const auto &getParameters() const noexcept
+    { return params; }
+    inline const BodyExpr &getBody() const noexcept
+    { return *body; }
   };
 
   class TraitExpr : public Expr {
@@ -167,9 +186,10 @@ namespace pfederc {
       std::vector<std::unique_ptr<FuncExpr>> &&functions) noexcept;
     virtual ~TraitExpr();
 
-    const Token &getIdentifier() const noexcept;
-    const Exprs &getTemplates() const noexcept;
-    const std::vector<std::unique_ptr<FuncExpr>> &getFunctions() const noexcept;
+    inline const Token &getIdentifier() const noexcept { return tokId; }
+    inline const auto &getTemplates() const noexcept { return templs; }
+    inline const auto &getFunctions() const noexcept
+    { return functions; }
   };
 
   class ClassExpr : public Expr {
@@ -187,11 +207,17 @@ namespace pfederc {
       std::vector<std::unique_ptr<FuncExpr>> &&functions) noexcept;
     virtual ~ClassExpr();
 
-    const Token &getIdentifier() const noexcept;
-    const Exprs &getTemplates() const noexcept;
-    const std::vector<std::unique_ptr<BiOpExpr>> &getConstructorAttributes() const noexcept;
-    const std::vector<std::unique_ptr<BiOpExpr>> &getAttributes() const noexcept;
-    const std::vector<std::unique_ptr<FuncExpr>> &getFunctions() const noexcept;
+    inline const Token &getIdentifier() const noexcept { return tokId; }
+    inline const auto &getTemplates() const noexcept { return templs; }
+
+    inline const auto &getConstructorAttributes() const noexcept
+    { return constructAttributes; }
+
+    inline const auto &getAttributes() const noexcept
+    { return attributes; }
+
+    inline const auto &getFunctions() const noexcept
+    { return functions; }
   };
 
   class TraitImplExpr : public Expr {
@@ -206,10 +232,13 @@ namespace pfederc {
         std::vector<std::unique_ptr<FuncExpr>> &&functions) noexcept;
     virtual ~TraitImplExpr();
 
-    const Token &getIdentifier() const noexcept;
-    const std::vector<std::unique_ptr<Expr>> &getTemplates() const noexcept;
-    const Expr &getImplementedTrait() const noexcept;
-    const std::vector<std::unique_ptr<FuncExpr>> &getFunctions() const noexcept;
+    inline const Token &getIdentifier() const noexcept { return classTokId; }
+    inline const std::vector<std::unique_ptr<Expr>> &getTemplates() const noexcept
+    { return templs; }
+    inline const Expr &getImplementedTrait() const noexcept
+    { return *implTrait; }
+    inline const auto &getFunctions() const noexcept
+    { return functions; }
   };
 
   typedef std::tuple<const Token&, std::vector<std::unique_ptr<Expr>>> EnumConstructor;
@@ -225,9 +254,214 @@ namespace pfederc {
         EnumConstructor &&contructors) noexcept;
     virtual ~EnumExpr();
 
-    const Token &getIdentifier() const noexcept;
-    const Exprs &getTemplates() const noexcept;
-    const std::vector<EnumConstructor> &getConstructors() const noexcept;
+    inline const Token &getIdentifier() const noexcept { return tokId; }
+    inline const auto &getTemplates() const noexcept { return templs; }
+    inline const auto &getConstructors() const noexcept
+    { return constructors; }
+  };
+
+  class ModExpr : public Expr {
+    const Token &tokId;
+    Exprs exprs;
+  public:
+    ModExpr(const Lexer &lexer, const Position &pos,
+        const Token &tokId, Exprs &&exprs) noexcept;
+    virtual ~ModExpr();
+
+    inline const Token &getIdentifier() const noexcept { return tokId; }
+    inline const auto &getExpressions() const noexcept { return exprs; }
+  };
+
+  class SafeExpr : public Expr {
+    std::unique_ptr<Expr> expr;
+  public:
+    SafeExpr(const Lexer &lexer, const Position &pos,
+        std::unique_ptr<Expr> &&expr) noexcept;
+    virtual ~SafeExpr();
+
+    /*!\return Returns memory allocation expression
+     * (array, class)
+     */
+    inline const Expr &getExpression() const noexcept { return *expr; }
+  };
+
+  typedef std::tuple<std::unique_ptr<Expr> /* cond */,
+      std::unique_ptr<BodyExpr>> IfCase;
+
+  class IfExpr : public Expr {
+    std::vector<IfCase> ifCases;
+    std::unique_ptr<BodyExpr> elseExpr;
+    bool isensure;
+  public:
+    /*!\brief Initializes IfExpr
+     * \param lexer
+     * \param pos
+     * \param ifCases size() >= 1
+     * \param elseExpr optional else expression
+     */
+    IfExpr(const Lexer &lexer, const Position &pos,
+        std::vector<IfCase> &&ifCases,
+        std::unique_ptr<BodyExpr> &&elseExpr, bool isensure = false) noexcept;
+    virtual ~IfExpr();
+
+    inline bool isEnsure() const noexcept { return isensure; }
+
+    inline const auto &getCases() const noexcept
+    { return ifCases; }
+
+    /*!\return Returns optional else clause
+     */
+    inline const BodyExpr *getElse() const noexcept { return elseExpr.get(); }
+  };
+
+  class LoopExpr : public Expr {
+    std::unique_ptr<Expr> initExpr, condExpr, itExpr;
+  public:
+    /*!\brief Initializes LoopExpr
+     * \param lexer
+     * \param type Either EXPR_LOOP_DO or EXPR_LOOP_FOR
+     * \param pos
+     * \param initExpr Optional init expression (statement)
+     * \param condExpr Loop continuation condition
+     * \param itExpr Optional iterator step expression
+     */
+    LoopExpr(const Lexer &lexer, ExprType type, const Position &pos,
+        std::unique_ptr<Expr> &&initExpr,
+        std::unique_ptr<Expr> &&condExpr,
+        std::unique_ptr<Expr> &&itExpr) noexcept;
+    virtual ~LoopExpr();
+
+    /*!\return Returns optional initialization expression
+     */
+    inline const Expr *getInitialization() const noexcept
+    { return initExpr.get(); }
+
+    /*!\return Returns loop continuation condition
+     */
+    inline const Expr &getCondition() const noexcept
+    { return *condExpr; }
+
+    /*!\return Returns optional iterate step expression
+     */
+    inline const Expr *getIterator() const noexcept
+    { return itExpr.get(); }
+  };
+
+  typedef std::tuple<const std::vector<Token *> /* constructors */,
+      const std::vector<const Token * /* variables */>,
+      std::unique_ptr<BodyExpr>> MatchPattern;
+
+  class MatchExpr : public Expr {
+    std::vector<MatchPattern> cases;
+    std::unique_ptr<BodyExpr> anyCase;
+  public:
+    /*!\brief Initializes MatchExpr
+     * \param lexer
+     * \param pos
+     * \param cases
+     * \param anyCase Optional any case
+     */
+    MatchExpr(const Lexer &lexer, const Position &pos,
+        std::vector<MatchPattern> &&cases,
+        std::unique_ptr<BodyExpr> &&anyCase) noexcept;
+    virtual ~MatchExpr();
+
+    inline const auto &getCases() const noexcept
+    { return cases; }
+
+    /*!\return Returns optional any case
+     */
+    const BodyExpr *getAnyCase() const noexcept
+    { return anyCase.get(); }
+  };
+
+  class BiOpExpr : public Expr {
+    const Token &tokOp;
+    std::unique_ptr<Expr> lhs, rhs;
+  public:
+    BiOpExpr(const Lexer &lexer, const Position &pos,
+        const Token &tokOp,
+        std::unique_ptr<Expr> &&lhs, std::unique_ptr<Expr> &&rhs) noexcept;
+    virtual ~BiOpExpr();
+
+    inline const Token &getOperator() const noexcept { return tokOp; }
+
+    inline const Expr &getRight() const noexcept { return *rhs; }
+
+    inline const Expr &getLeft() const noexcept { return *lhs; }
+  };
+
+  class UnOpExpr : public Expr {
+    const Token &tokOp;
+    std::unique_ptr<Expr> expr;
+  public:
+    UnOpExpr(const Lexer &lexer, const Position &pos,
+        const Token &tokOp, std::unique_ptr<Expr> &&expr) noexcept;
+    virtual ~UnOpExpr();
+
+    inline const Token &getOperator() const noexcept { return tokOp; }
+
+    inline const Expr &getExpression() const noexcept { return *expr; }
+  };
+
+  class BodyExpr : public Expr {
+    Exprs exprs;
+    std::unique_ptr<Expr> retExpr;
+  public:
+    BodyExpr(const Lexer &lex, const Position &pos,
+        Exprs &&exprs, std::unique_ptr<Expr> &&retExpr) noexcept;
+    virtual ~BodyExpr();
+
+    inline const auto &getExpressions() const noexcept { return exprs; }
+
+    /*!\brief Optional return statement
+     */
+    inline const Expr *getReturn() const noexcept { return retExpr.get(); }
+  };
+
+  class ArrayCpyExpr : public Expr {
+    std::unique_ptr<Expr> valueExpr, lengthExpr;
+  public:
+    ArrayCpyExpr(const Lexer &lexer, const Position &pos,
+        std::unique_ptr<Expr> &&valuexpr,
+        std::unique_ptr<Expr> &&lengthExpr) noexcept;
+    virtual ~ArrayCpyExpr();
+
+    inline const Expr &getValue() const noexcept { return *valueExpr; }
+    inline const Expr &getLength() const noexcept { return *lengthExpr; }
+  };
+
+  class ArrayLitExpr : public Expr {
+    Exprs exprs;
+  public:
+    /*!\brief Initializes ArrayLitExpr
+     * \param lexer
+     * \param pos
+     * \param expr0 Ensure at least two expressions
+     * \param expr1 Ensure at least two expressions
+     * \param exprs Does not start with neither contain expr0, expr1
+     */
+    ArrayLitExpr(const Lexer &lexer, const Position &pos,
+        std::unique_ptr<Expr> &&expr0,
+        std::unique_ptr<Expr> &&expr1,
+        Exprs &&exprs) noexcept;
+    virtual ~ArrayLitExpr();
+
+    /*!\return Returns the array's values
+     */
+    inline const auto &getValues() const noexcept { return exprs; }
+  };
+
+  class ArrayEmptyExpr : public Expr {
+    std::unique_ptr<Expr> typeExpr;
+  public:
+    ArrayEmptyExpr(const Lexer &lexer, const Position &pos,
+        std::unique_ptr<Expr> &&typeExpr) noexcept;
+    virtual ~ArrayEmptyExpr();
+
+    /*!\return Returns type of every element in the array
+     */
+    inline const Expr &getType() const noexcept { return *typeExpr; }
   };
 }
 
--- libs/pfederc_syntax/src/expr.cpp
+++ libs/pfederc_syntax/src/expr.cpp
@@ -1,23 +1,231 @@
 #include "pfederc/expr.hpp"
 using namespace pfederc;
 
-// Expr
-
-Expr::Expr(const Lexer &lexer, ExprType type, const Position &pos) noexcept
-    : lexer{lexer}, type{type}, pos(pos) {
+// ProgramExpr
+ProgramExpr::ProgramExpr(const Lexer &lexer, const Position &pos,
+    const Token *progName,
+    Exprs &&imports,
+    Exprs &&defs) noexcept
+    : Expr(lexer, EXPR_PROG, pos),
+      imports(std::move(imports)),
+      defs(std::move(defs)) {
 }
 
-Expr::~Expr() {
+ProgramExpr::~ProgramExpr() {
 }
 
-const Lexer &Expr::getLexer() const noexcept {
-  return lexer;
+// TokenExpr
+TokenExpr::TokenExpr(const Lexer &lexer, const Token &tok) noexcept
+    : Expr(lexer, EXPR_TOK, tok.getPosition()), tok{tok} {
 }
 
-ExprType Expr::getType() const noexcept {
-  return type;
+TokenExpr::~TokenExpr() {
+
 }
 
-const Position &Expr::getPosition() const noexcept {
-  return pos;
+// UseExpr
+UseExpr::UseExpr(const Lexer &lexer, const Position &pos,
+    std::unique_ptr<Expr> &&expr) noexcept
+    : Expr(lexer, EXPR_USE, pos), expr(std::move(expr)) {
 }
+
+UseExpr::~UseExpr() {
+
+}
+
+// FuncExpr
+FuncExpr::FuncExpr(const Lexer &lexer, const Position &pos,
+    const Token &tokId,
+    Exprs &&templ,
+    std::vector<FuncParameter> &&params,
+    std::unique_ptr<Expr> &&returnExpr,
+    std::unique_ptr<BodyExpr> &&body) noexcept
+    : Expr(lexer, EXPR_FUNC, pos), tokId{tokId},
+      templs(std::move(templs)), params(std::move(params)),
+      returnExpr(std::move(returnExpr)), body(std::move(body)) {
+}
+
+ FuncExpr::~FuncExpr() {
+}
+
+// LambdaExpr
+LambdaExpr::LambdaExpr(const Lexer &lexer, const Position &pos,
+    Exprs &&params,
+    std::unique_ptr<BodyExpr> &&body) noexcept
+    : Expr(lexer, EXPR_LAMBDA, pos),
+      params(std::move(params)), body(std::move(body)) {
+}
+
+ LambdaExpr::~LambdaExpr() {
+}
+
+// TraitExpr
+TraitExpr::TraitExpr(const Lexer &lexer, const Position &pos,
+    const Token &tokId,
+    Exprs &&templs,
+    std::vector<std::unique_ptr<FuncExpr>> &&functions) noexcept
+    : Expr(lexer, EXPR_TRAIT, pos), tokId{tokId},
+      templs(std::move(templs)), functions(std::move(functions)) {
+}
+
+ TraitExpr::~TraitExpr() {
+}
+
+// ClassExpr
+ClassExpr::ClassExpr(const Lexer &lexer, const Position &pos,
+    const Token &tokId,
+    Exprs &&templs,
+    std::vector<std::unique_ptr<BiOpExpr>> &&constructAttributes,
+    std::vector<std::unique_ptr<BiOpExpr>> &&attributes,
+    std::vector<std::unique_ptr<FuncExpr>> &&functions) noexcept
+    : Expr(lexer, EXPR_CLASS, pos), tokId{tokId},
+      templs(std::move(templs)),
+      constructAttributes(std::move(constructAttributes)),
+      attributes(std::move(attributes)),
+      functions(std::move(functions)) {
+}
+
+ ClassExpr::~ClassExpr() {
+}
+
+// TraitImplExpr
+TraitImplExpr::TraitImplExpr(const Lexer &lexer, const Position &pos,
+    const Token &classTokId, Exprs &&templs,
+    std::unique_ptr<Expr> &&implTrait,
+    std::vector<std::unique_ptr<FuncExpr>> &&functions) noexcept
+    : Expr(lexer, EXPR_TRAITIMPL, pos), classTokId{classTokId},
+      templs(std::move(templs)),
+      implTrait(std::move(implTrait)), functions(std::move(functions)) {
+}
+
+ TraitImplExpr::~TraitImplExpr() {
+}
+
+// EnumExpr
+EnumExpr::EnumExpr(const Lexer &lexer, const Position &pos,
+    const Token &tokId,
+    Exprs &&templs,
+    EnumConstructor &&contructors) noexcept
+    : Expr(lexer, EXPR_ENUM, pos), tokId{tokId},
+      templs(std::move(templs)), constructors(std::move(constructors)) {
+}
+
+ EnumExpr::~EnumExpr() {
+}
+
+// ModExpr
+ModExpr::ModExpr(const Lexer &lexer, const Position &pos,
+    const Token &tokId, Exprs &&exprs) noexcept
+    : Expr(lexer, EXPR_MOD, pos), tokId{tokId}, exprs(std::move(exprs)) {
+}
+
+ModExpr::~ModExpr() {
+}
+
+// SafeExpr
+SafeExpr::SafeExpr(const Lexer &lexer, const Position &pos,
+    std::unique_ptr<Expr> &&expr) noexcept
+    : Expr(lexer, EXPR_SAFE, pos), expr(std::move(expr)) {
+}
+
+SafeExpr::~SafeExpr() {
+}
+
+// IfExpr
+IfExpr::IfExpr(const Lexer &lexer, const Position &pos,
+    std::vector<IfCase> &&ifCases,
+    std::unique_ptr<BodyExpr> &&elseExpr, bool isensure) noexcept
+    : Expr(lexer, EXPR_IF, pos), ifCases(std::move(ifCases)),
+      elseExpr(std::move(elseExpr)), isensure{isensure} {
+}
+
+IfExpr::~IfExpr() {
+}
+
+// LoopExpr
+LoopExpr::LoopExpr(const Lexer &lexer, ExprType type, const Position &pos,
+    std::unique_ptr<Expr> &&initExpr,
+    std::unique_ptr<Expr> &&condExpr,
+    std::unique_ptr<Expr> &&itExpr) noexcept
+    : Expr(lexer, type, pos), initExpr(std::move(initExpr)),
+      condExpr(std::move(condExpr)), itExpr(std::move(itExpr)) {
+}
+
+LoopExpr::~LoopExpr() {
+}
+
+// MatchExpr
+MatchExpr::MatchExpr(const Lexer &lexer, const Position &pos,
+    std::vector<MatchPattern> &&cases,
+    std::unique_ptr<BodyExpr> &&anyCase) noexcept
+    : Expr(lexer, EXPR_MATCH, pos),
+      cases(std::move(cases)),
+      anyCase(std::move(anyCase)) {
+}
+
+MatchExpr::~MatchExpr() {
+}
+
+// BiOpExpr
+BiOpExpr::BiOpExpr(const Lexer &lexer, const Position &pos,
+     const Token &tokOp,
+     std::unique_ptr<Expr> &&lhs, std::unique_ptr<Expr> &&rhs) noexcept
+     : Expr(lexer, EXPR_BIOP, pos), tokOp{tokOp},
+       lhs(std::move(lhs)), rhs(std::move(rhs)) {
+}
+
+BiOpExpr::~BiOpExpr() {
+}
+
+// UnOpExpr
+UnOpExpr::UnOpExpr(const Lexer &lexer, const Position &pos,
+     const Token &tokOp, std::unique_ptr<Expr> &&expr) noexcept
+     : Expr(lexer, EXPR_UNOP, pos), tokOp{tokOp}, expr(std::move(expr)) {
+}
+
+UnOpExpr::~UnOpExpr() {
+}
+
+// BodyExpr
+BodyExpr::BodyExpr(const Lexer &lex, const Position &pos,
+     Exprs &&exprs, std::unique_ptr<Expr> &&retExpr) noexcept
+     : Expr(lex, EXPR_BODY, pos), exprs(std::move(exprs)),
+       retExpr(std::move(retExpr)) {
+}
+
+BodyExpr::~BodyExpr() {
+}
+
+// ArrayCpyExpr
+ArrayCpyExpr::ArrayCpyExpr(const Lexer &lexer, const Position &pos,
+     std::unique_ptr<Expr> &&valuExpr,
+     std::unique_ptr<Expr> &&lengthExpr) noexcept
+     : Expr(lexer, EXPR_ARRCPY, pos), valueExpr(std::move(valuExpr)),
+       lengthExpr(std::move(lengthExpr)) {
+}
+
+ ArrayCpyExpr::~ArrayCpyExpr() {
+}
+
+// ArrayLitExpr
+ArrayLitExpr::ArrayLitExpr(const Lexer &lexer, const Position &pos,
+     std::unique_ptr<Expr> &&expr0,
+     std::unique_ptr<Expr> &&expr1,
+     Exprs &&exprs) noexcept
+     : Expr(lexer, EXPR_ARRLIT, pos),
+       exprs(std::move(exprs)) {
+  exprs.insert(exprs.begin(), std::move(expr1));
+  exprs.insert(exprs.begin(), std::move(expr0));
+}
+
+ArrayLitExpr::~ArrayLitExpr() {
+}
+
+// ArrayEmptyExpr
+ArrayEmptyExpr::ArrayEmptyExpr(const Lexer &lexer, const Position &pos,
+     std::unique_ptr<Expr> &&typeExpr) noexcept
+     : Expr(lexer, EXPR_ARREMPTY, pos), typeExpr(std::move(typeExpr)) {
+}
+
+ ArrayEmptyExpr::~ArrayEmptyExpr() {
+ }
